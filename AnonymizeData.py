8c158fc9fc13e7cab2733695c1ea01388d8b637b
Pamanji.Dinesh <pamanji.dinesh@bcg.com>
2019-03-13 15:58:17 +0530

    Anonymize columns - Faker - All files in a directory

--- AnonymizeData.py
+++ AnonymizeData.py
@@ -1,3 +1,13 @@
+# User inputs
+path = 'C:/Projects/MarketCatalyst/'
+output_path = path+'AnonymizedToolInputs/'
+# Dictionary with column name to be anonymized and faker dictionary to be used
+col_faker_dict = {'Brand': brand, 'SubBrand': subBrand, 'GrowthDriver': growthDriver, 'Geography': geo, 'State': state,
+                  'ReceivingBrand': brand, 'ReceivingSubBrand': subBrand, 'ReceivingGeography': geo}
+
+##############################################################################################################
+##############################################################################################################
+
 def install_and_import(package):
     import importlib
     try:
@@ -8,7 +18,18 @@ def install_and_import(package):
     finally:
         globals()[package] = importlib.import_module(package)
 
-results = map(install_and_import, ('pandas', 'faker', 'collections', 'unicodecsv'))
+
+def anonymize_faker(df, col, fakerdict):
+    append_list = []
+    for i in range(len(df[col])):
+        # Replace the field with faked fields.
+        value = fakerdict[df[col][i]]
+        append_list.append(value)
+    df[col] = pd.Series(append_list)
+    return df
+
+# Import packages
+results = map(install_and_import, ('os','pandas', 'faker', 'collections', 'unicodecsv', 'glob'))
 set(results)
 
 import pandas as pd
@@ -18,33 +39,37 @@ from collections import defaultdict
 
 
 # Load the faker and its providers
-faker  = Factory.create()
+faker = Factory.create()
 # Create mappings of names & emails to faked names & emails.
-brand  = defaultdict(faker.company)
+brand = defaultdict(faker.company)
 subBrand = defaultdict(faker.street_name)
 geo = defaultdict(faker.country)
 growthDriver = defaultdict(faker.catch_phrase)
+state = defaultdict(faker.city)
 
-path = 'C:/Projects/MarketCatalyst/'
+# Get list of files in the directory
+os.chdir(path)
+files_list = [i for i in glob.glob('*.{}'.format('csv'))]
 
-source = path+"3. Curves.csv"
-target = path+"Test_fake.csv"
+# Get two lists with column names and faker dictionary from the dictionary
+columns = list(col_faker_dict.keys())
+faker_dict = list(col_faker_dict.values())
 
-df = pd.read_csv(source)
+# Loop thru all the files in the directory
+for file in files_list:
+    # print(path+file)
+    df = pd.read_csv(path+file)
 
-def anonymize_faker(df, col, fakerdict):
+    # Loop thru all the columns to be anonymized
+    for col in range(len(columns)):
+        try:
+            if len(df[columns[col]]) > 0:
+                print('Unique rows in [%s] before anonymization: %d' % (columns[col],df[columns[col]].nunique()))
+                df = anonymize_faker(df, columns[col], faker_dict[col])
+                print('Unique rows in [%s] after anonymization: %d' % (columns[col],df[columns[col]].nunique()))
+        except:
+            print('Column [%s] not present in [%s]' % (columns[col], file))
 
-    append_list = []
-    for i in range(len(df[col])):
-        # Replace the field with faked fields.
-        value = fakerdict[df[col][i]]
-        append_list.append(value)
-    df[col] = pd.Series(append_list)
-    return df
+    # Write anonymized tables to csv
+    df.to_csv(output_path+file)
 
-
-# Convert fields with fake names
-df = anonymize_faker(df, 'Brand', brand)
-df = anonymize_faker(df, 'ReceivingSubBrand', subBrand)
-df = anonymize_faker(df, 'Instrument', growthDriver)
-df = anonymize_faker(df, 'Geography', geo)
